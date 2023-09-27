c5b71b60c12305062738cc02f2fd4670eca277ef
Jensvds <jens.vanderstraeten@student.howest.be>
2023-09-27 15:34:07 +0200

    First commit

--- script/app.js
+++ script/app.js
@@ -1,5 +1,14 @@
 const handlePasswordSwitcher = function () {
     console.log("handlePasswordSwitcher executed");
+
+    const passwordOptions = ['password', 'text'];
+
+    const passwordInput = document.querySelector('.js-password-input');
+    const passwordToggle = document.querySelector('.js-password-toggle-checkbox');
+
+    passwordToggle.addEventListener('change', function () {
+        passwordInput.type = passwordOptions[+this.checked];
+    });
 }
 
 const init = function () {
